4c05a309ca68ea05ee0ff80ab12bcb4da341d0a7
Dawid Wiśniewski <dw949@wp.pl>
2024-06-01 10:22:57 +0000

    feat: update node register

--- src/setup/nodeTypes.ts
+++ src/setup/nodeTypes.ts
@@ -1,8 +1,11 @@
-import { GenerateNode } from "../nodes/Generate";
+import { GenerateNode, GenerateNode3 } from "../nodes/GenerateNode";
 import Graph from "../Graph";
 import { InputNode } from "../nodes/InputNode";
+import { OutputNode } from "../nodes/OutputNode";
 
 export default () => {
   Graph.registerNode("input", InputNode);
   Graph.registerNode("generate", GenerateNode);
+  Graph.registerNode("generate3", GenerateNode3);
+  Graph.registerNode("output", OutputNode);
 };
